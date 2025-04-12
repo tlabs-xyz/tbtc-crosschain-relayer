9353f53b6a54ef273d324b8956d30c59fcf510b2
evandrosaturnino <evandro.saturnino@gmail.com>
2025-04-12 17:39:54 -0300

    fix: call compression function

--- index.ts
+++ index.ts
@@ -52,7 +52,7 @@ app.disable('x-powered-by');
 // -------------------------------------------------------------------------
 
 // Compresion
-app.use(compression as any);
+app.use(compression() as any);
 
 // File Upload limit
 app.use(express.json({ limit: '2048mb' }));
