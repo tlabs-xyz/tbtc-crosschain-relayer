141d7937bb4d58999a050ea44636b9776e1f020e
Piotr Roslaniec <p.roslaniec@gmail.com>
2025-06-10 16:46:54 +0200

    fix: print stringified error when config validation fails

--- config/app.config.ts
+++ config/app.config.ts
@@ -1,16 +1,14 @@
-import { z } from 'zod';
 import logger from '../utils/Logger.js';
 import { AppConfigSchema, type AppConfig } from './schemas/app.schema.js';
+import fs from 'fs';
 
 export const appConfig: AppConfig = (() => {
   try {
     return AppConfigSchema.parse(process.env);
   } catch (error: any) {
-    if (error instanceof z.ZodError) {
-      logger.error('Application configuration validation failed:', error.flatten());
-    } else {
-      logger.error('An unexpected error occurred while loading application configuration:', error);
-    }
+    const errorAsString = JSON.stringify(error, null, 2);
+    fs.writeFileSync('error.log', errorAsString);
+    logger.error('Application configuration validation failed:', errorAsString);
     process.exit(1);
   }
 })();
